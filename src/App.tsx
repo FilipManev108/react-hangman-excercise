b94513e152edd0b47e2a58bdc3d82fe0bf4a9941
Filip Manev <7605817-FilipManev@users.noreply.gitlab.com>
2023-03-17 16:40:37 +0100

    keyboard logic in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import words from "./wordList.json";
 
 import HangmanDrawing from "./HangmanDrawing";
@@ -14,6 +14,26 @@ function App() {
   const incorrectLetters = guessedLetters.filter(
     (letter) => !wordToGuess.includes(letter)
   );
+
+  const addGuessedLetter = (key: string) => {
+    if (guessedLetters.includes(key)) return;
+    setGuessedLetters((currentLetters) => [...currentLetters, key]);
+  };
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+
+      addGuessedLetter(key);
+    };
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, []);
   console.log(wordToGuess);
   return (
     <div
